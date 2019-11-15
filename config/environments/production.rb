9a07c18e07072c3fe628a0f0302043e28f122de3
adelola <lola.adekunle@gmail.com>
2019-11-15 14:00:55 -0600

    Add clearance gem and users table

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,6 +60,9 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "a_b_c_development_production"
 
+  config.action_mailer.default_url_options = { host: 'abcdevelopment.xyz' }
+
+
   config.action_mailer.perform_caching = false
 
   # Ignore bad email addresses and do not raise email delivery errors.
