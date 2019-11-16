84e43e8d6ca048eb1705b5113374ac7acaf27263
adelola <lola.adekunle@gmail.com>
2019-11-16 05:54:24 -0600

    Add Home controller and view

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,9 @@
 class User < ApplicationRecord
   include Clearance::User
 
-  include Clearance::User
+  validates :name, presence: true
+  validates :email, presence: true, uniqueness: true
+
+  # has_many :classrooms, dependent: :destroy
+  # has_many :students, through: :classrooms 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,24 @@
 Rails.application.routes.draw do
+  constraints Clearance::Constraints::SignedIn.new do
+    # root to: "dashboards#show"
+  end
+  
+  root to: "homes#show"
+  
+  # resources :classrooms
+  # resources :students
+  # resources :challenges, only: [:edit, :update, :delete]
+  resources :passwords, controller: "clearance/passwords", only: [:create, :new]
+  resource :session, controller: "clearance/sessions", only: [:create]
+
+  resources :users, only: [:create] do
+    resource :password,
+      controller: "clearance/passwords",
+      only: [:create, :edit, :update]
+  end
+
+  get "/sign_in" => "clearance/sessions#new", as: "sign_in"
+  delete "/sign_out" => "clearance/sessions#destroy", as: "sign_out"
+  get "/sign_up" => "clearance/users#new", as: "sign_up"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
