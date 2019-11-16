001aa43443e6b1d4aac02ed6ed6602e8fe62e3f4
adelola <lola.adekunle@gmail.com>
2019-11-16 13:17:46 -0600

    Add dashboard for authenticated user

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
   constraints Clearance::Constraints::SignedIn.new do
-    # root to: "dashboards#show"
+    root to: "dashboards#show" , as: :authenticated_root
   end
   
   root to: "homes#show"
   
-  # resources :classrooms
-  # resources :students
-  # resources :challenges, only: [:edit, :update, :delete]
+  resources :classrooms
+  resources :students
+  resources :challenges, only: [:edit, :update, :delete]
   resources :passwords, controller: "clearance/passwords", only: [:create, :new]
   resource :session, controller: "clearance/sessions", only: [:create]
 
@@ -20,5 +20,4 @@ Rails.application.routes.draw do
   get "/sign_in" => "clearance/sessions#new", as: "sign_in"
   delete "/sign_out" => "clearance/sessions#destroy", as: "sign_out"
   get "/sign_up" => "clearance/users#new", as: "sign_up"
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
