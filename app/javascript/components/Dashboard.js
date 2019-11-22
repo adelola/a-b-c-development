40db7ceafda9db30453fbd3550ef4ab865e5b70a
adelola <lola.adekunle@gmail.com>
2019-11-22 17:11:26 -0600

    Add sidebar to dashboard view

--- app/javascript/components/Dashboard.js
+++ app/javascript/components/Dashboard.js
@@ -10,12 +10,18 @@ class Dashboard extends React.Component {
   render () {
     return (
       <div className={styles.dashboard}>
-        <Switch>
-          <Route exact path="/" component={ClassroomsIndex} />
-          <Route exact path="/classrooms" component={ClassroomsIndex} />
-          <Route exact path="/classrooms/:id" component={ClassroomShow} />
-          <Route exact path="/students/:id" component={StudentShow} />
-        </Switch>
+        <aside className={styles.sidebar}>
+          <h1>Sidebar</h1>
+          Classrooms
+        </aside>
+        <article className={styles.content}>
+          <Switch>
+            <Route exact path="/" component={ClassroomsIndex} />
+            <Route exact path="/classrooms" component={ClassroomsIndex} />
+            <Route exact path="/classrooms/:id" component={ClassroomShow} />
+            <Route exact path="/students/:id" component={StudentShow} />
+          </Switch>
+        </article>
       </div>
     )
   }
