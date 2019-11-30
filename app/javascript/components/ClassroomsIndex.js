708c5df00922decfa55ace33af41db00b4d5ee7d
adelola <lola.adekunle@gmail.com>
2019-11-30 14:32:37 -0600

    Add list of classrooms to ClassroomIndex

--- app/javascript/components/ClassroomsIndex.js
+++ app/javascript/components/ClassroomsIndex.js
@@ -1,18 +1,42 @@
 import React, { useState, useEffect } from 'react';
 import Axios from 'axios';
+import CreateClassroom from './forms/CreateClassroom';
+import { Link } from 'react-router-dom';
 
 const ClassroomsIndex = () => {
+ const [classrooms, setClassrooms] = useState([]);
+ const [isLoading, setIsLoading] = useState(true);
  
- useEffect( () => {
-    console.log('something is happening')
-    Axios.get('/api/classrooms')
-    .then(response => console.log(response.data))
-      
+  useEffect(() => {
+    setIsLoading(false);
+    const fetchData = async () => {
+      const result = await Axios.get('/api/classrooms');
+      setClassrooms([...result.data]);
+      console.log(`Retrieved ${result.data.length} classrooms`);
+      setIsLoading(false);
+    };
+  
+    fetchData();
   }, []);
 
   return (
     <React.Fragment>
+      {isLoading && <p>
+        <span className="sr-only">Loading...</span>
+      </p> }
       <h1>ClassroomsIndex</h1>
+      <ul>
+        {classrooms.map(( node ) => {
+          return (
+            <li key={node.id}>
+              <Link to={`/classrooms/${node.id}`} >{node.name}</Link>
+            </li>
+          )})
+        }
+      </ul><br /> <br />
+      <div>
+        <CreateClassroom />
+      </div>
     </React.Fragment>
   )
   
--- app/javascript/components/Dashboard.js
+++ app/javascript/components/Dashboard.js
@@ -18,7 +18,7 @@ class Dashboard extends React.Component {
         <article className={styles.content}>
           <Switch>
             <Route exact path="/" component={ClassroomsIndex} />
-            <Route path="/classrooms" component={ClassroomsIndex} />
+            <Route exact path="/classrooms" component={ClassroomsIndex} />
             <Route path="/classrooms/:id" component={ClassroomShow} />
             <Route path="/students/:id" render={()=>{
                 return <StudentShow text="word" />
