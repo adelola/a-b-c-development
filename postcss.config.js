83ef39ca90a0cd7f65f892bf9b545f7ab9c19476
adelola <lola.adekunle@gmail.com>
2020-01-25 23:57:08 -0600

    Add purgecss to minimize css file size

--- postcss.config.js
+++ postcss.config.js
@@ -1,3 +1,13 @@
+const purgecss = require('@fullhuman/postcss-purgecss')({
+  // Specify the paths to all of the template files in your project 
+  content: [
+    './src/**/*.html',
+    './src/**/*.jsx',
+  ],
+  // Include any special characters you're using in this regular expression
+  defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
+})
+
 module.exports = {
   plugins: [
     require('autoprefixer'),
@@ -10,5 +20,8 @@ module.exports = {
       stage: 3
     }),
     require('tailwindcss'),
+    ...process.env.NODE_ENV === 'production'
+      ? [purgecss]
+      : []
   ]
 }
