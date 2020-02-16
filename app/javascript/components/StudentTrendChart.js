a3442e1a7033105dc285c226d41156512df8687a
adelola <lola.adekunle@gmail.com>
2020-02-15 20:14:27 -0600

    Update student trend chart to area chart

--- app/javascript/components/StudentTrendChart.js
+++ app/javascript/components/StudentTrendChart.js
@@ -1,8 +1,9 @@
 import React, { useEffect } from 'react';
-import { LineChart, Line, CartesianGrid, XAxis, YAxis, Tooltip } from 'recharts';
+import { AreaChart, Area, linearGradient, CartesianGrid, XAxis, YAxis, Tooltip } from 'recharts';
 
 const StudentTrendChart = (props) => {
-
+  
+    console.log(props)
   // const CustomizedDot = (props) => {
   //   const {
   //     cx, cy, stroke, payload, value,
@@ -23,15 +24,24 @@ const StudentTrendChart = (props) => {
   //   );
   // };
 
+
   const renderLineChart = (
-    <LineChart width={400} height={400} data={props.data} margin={{ top: 5, right: 5, bottom: 5, left: 20 }}>
+    <AreaChart width={400} height={400} data={props.data} margin={{ top: 5, right: 5, bottom: 5, left: 5 }}>
       {/* <Line type="monotone" dataKey="score" stroke="#8884d8" isAnimationActive={false} dot={<CustomizedDot />}/> */}
-      <Line type="monotone" dataKey="score" stroke="#8884d8" isAnimationActive={false} />
-      <CartesianGrid stroke="#ccc" strokeDasharray="2 2" />
+      {/* <Line type="monotone" dataKey="score" stroke="#8884d8" isAnimationActive={false} /> */}
+      <defs>
+        <linearGradient id="splitColor" x1="0" y1="0" x2="0" y2="1">
+            <stop offset="1" stopColor="green" stopOpacity={0.6} />
+            {/* <stop offset="50%" stopColor="red" stopOpacity={1} /> */}
+        </linearGradient>
+      </defs>
+      <CartesianGrid stroke="#ccc " strokeDasharray="1 1" />
       <XAxis dataKey="date" />
       <YAxis />
       <Tooltip />
-    </LineChart>
+     
+      <Area type="monotone" dataKey="score" stroke="#000" fill="url(#splitColor)" dot={{ stroke: 'black', strokeWidth: 1 }} isAnimationActive={true} animationEasing="ease-in-out" />
+    </AreaChart>
   );
 
   return(
