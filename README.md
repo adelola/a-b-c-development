6a2c8df8c37496d1919f52b9c54f4ae7fdf49c92
adelola <lola.adekunle@gmail.com>
2020-02-18 19:39:01 -0600

    Update main header styling

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # README
 
+##Attributions
+'Hi' icon on main header when signed in - hindi by Jonathan Coutiño from the Noun Project
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
   include Clearance::Controller
+
+  def svg(name)
+    file_path = "#{Rails.root}/app/assets/images/#{name}.svg"
+    return File.read(file_path).html_safe if File.exists?(file_path)
+    '(not found)'
+  end
+  
 end
