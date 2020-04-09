e8963ff427150df3c4f96c6d9637c759ace616ad
adelola <lola.adekunle@gmail.com>
2020-04-08 19:39:36 -0500

    Add alphabet style flow

--- app/javascript/components/StudentItem.js
+++ app/javascript/components/StudentItem.js
@@ -1,17 +1,26 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import { Link } from 'react-router-dom';
 import styles from './../stylesheets/components/studentitem'; 
 
 
 const StudentItem = (props) => { 
     const name = props.name;
-    const id = props.id
     const lastScore = props.lastScore
     const handleDelete = props.handleDelete;
+    const [initial, setInitial] = useState(""); 
+
+    const getInitial = (name) =>{
+      const letter = name.toLowerCase().match(/\b[a-z]/g)[0];  //Gets student's first initial
+      setInitial("letter_"+letter);                             //Builds statement to match appropriate class name based on initials
+    }
+    
+    useEffect(() => {
+      getInitial(name);
+    }, []);
+
 
     return (
-      <div className={styles.item}>
-        {/* <Link to={`/students/${id}`}>{name}</Link> &nbsp; &nbsp; &nbsp; */}
+      <div className={`${styles.item} ${initial}`}>
         <h1>{name}</h1>
         <span>Recent Challenge: {lastScore} &nbsp; &nbsp; &nbsp; </span>
         <button type="button" onClick={handleDelete}>Delete Student</button>
